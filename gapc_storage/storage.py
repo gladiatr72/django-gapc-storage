0abce9f078bc78bdcdfbc5b0a9b4d50790e841a0
Stephen Spencer <stephen@revsys.com>
2018-11-16 07:53:15 +0000

    grab updates from upstream

--- gapc_storage/storage.py
+++ gapc_storage/storage.py
@@ -91,6 +91,7 @@ class GoogleCloudStorage(Storage):
         self.path_prefix = self.path_prefix if hasattr(self, "path_prefix") else config["path_prefix"]
         self.allow_overwrite = self.allow_overwrite if hasattr(self, "allow_overwrite") else config["allow_overwrite"]
         self.cache_control = self.cache_control if hasattr(self, "cache_control") else config["cache_control"]
+        self.num_retries = self.num_retries if hasattr(self, "num_retries") else config["num_retries"]
 
     def _gcs_file_storage_settings(self):
         config_env = "GAPC_{}_STORAGE".format(self.name.upper())
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-gapc-storage",
-    version="0.4.4",
+    version="0.4.6",
     author="Eldarion, Inc.",
     author_email="development@eldarion.com",
     description="a Django storage backend using GCS JSON API",
@@ -12,7 +12,7 @@ setup(
     url="http://github.com/eldarion/django-gapc-storage",
     packages=find_packages(),
     install_requires=[
-        "google-api-python-client>=1.5.0",
+        "google-api-python-client>=1.5.0,<=1.7",
         "oauth2client",
         "python-dateutil",
     ],
